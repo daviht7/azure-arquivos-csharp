75fd93a78b4a3c1550762512304f7758d7612340
Davi Holanda <daviht7@gmail.com>
2023-07-30 18:37:23 -0300

    projeto finalizado

---
+++ BlobDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorageAPI
+{
+  public class BlobDto
+  {
+
+    public string Nome { get; set; }
+    public string Tipo { get; set; }
+    public string Uri { get; set; }
+
+  }
+}
